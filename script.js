8a8952ae3658fd486073c16a5f6c00aaf1906245
SubashchandrabossP9788 <sp6381449@gmail.com>
2024-05-29 09:48:29 +0530

    add js

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ var sidenavbar = document.querySelector(".side-navbar")
 sideNavMenu.addEventListener("click",function(){
     sidenavbar.style.marginLeft="0px"
 })
-
 document.getElementById("side-navbar-close").addEventListener("click",()=>{
     document.querySelector(".side-navbar").style.marginLeft = "-60%"
 })
